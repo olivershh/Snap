e886a7df9888d72f34459f20f876a5128c751f1b
Ibraheem <ibraheemalmadini@gmail.com>
2022-10-12 16:14:02 +0100

    albums page

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
+import 'react-native-gesture-handler'
 import { StyleSheet, Text, View, SafeAreaView, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { useState } from "react";
 import Login from "./screens/Login";
 import Profile from "./screens/Profile";
-import Albums from "./screens/Albums";
 import Camera from "./screens/Camera";
+import AlbumStack from './stacks/AlbumStack';
 
 const Tab = createBottomTabNavigator();
 
@@ -20,7 +21,7 @@ export default function App() {
         <NavigationContainer style={{ flex: 1 }}>
           <Tab.Navigator>
             <Tab.Screen name="Camera" component={Camera} />
-            <Tab.Screen name="Albums" component={Albums} />
+            <Tab.Screen name="Album Stack" component={AlbumStack} options={{ headerShown: false }} />
             <Tab.Screen
               name="Profile"
               children={() => <Profile setHasUser={setHasUser} />}
