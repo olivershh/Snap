4671ea40a7c307d31d65955dee5f0c874fdf2e36
olivershh <109047037+olivershh@users.noreply.github.com>
2022-10-12 17:28:50 +0100

    Revert "Albums page"

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import 'react-native-gesture-handler'
 import { StyleSheet, Text, View, SafeAreaView, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "./screens/Login";
 import Profile from "./screens/Profile";
-import AlbumStack from './stacks/AlbumStack';
+import Albums from "./screens/Albums";
 import CameraScreen from "./screens/CameraScreen";
 import SinglePhotoScreen from "./screens/SinglePhotoScreen";
 
@@ -15,14 +14,12 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   const TabNav = () => {
     return (
-
       <Tab.Navigator screenOptions={{ headerShown: true }}>
         <Tab.Screen name="Camera" component={CameraScreen} />
         <Tab.Screen name="Albums" component={Albums} />
         <Tab.Screen name="Profile" children={() => <Profile />} />
         <Tab.Screen name="TempSingle" component={SinglePhotoScreen} />
       </Tab.Navigator>
-
     );
   };
 
