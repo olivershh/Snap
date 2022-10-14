0338c42910b3eee3f9bf39393400b303ef42ccd0
JOSH SPENCE <joshua.spence@hotmail.co.uk>
2022-10-14 11:12:01 +0100

    updated film widget

--- components/Film.js
+++ components/Film.js
@@ -1,9 +1,12 @@
-import {View, Text} from "react-native";
+import { View, Text } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
 export default function Film(props) {
   console.log(props.film.name);
   return (
     <View>
       <Text>{props.film.name}</Text>
+      <MaterialCommunityIcons name="film" size={100} color="black" />
       <Text>
         {props.film.photosTaken}/{props.film.size}
       </Text>
