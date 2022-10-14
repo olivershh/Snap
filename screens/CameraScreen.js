0f318c1b387ff4f676585b676566dea467c5ebd0
olivershh <oliver.shackleton@gmail.com>
2022-10-14 16:46:37 +0100

    changed to cork bg

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -11,7 +11,7 @@ export default function CameraScreen() {
 
   return (
     <ImageBackground
-      source={require("../colors.jpg")}
+      source={require("../cork.jpeg")}
       style={{ padding: 15, backgroundColor: "gray", flex: 1 }}
     >
       <View style={{ padding: 15, backgroundColor: "white" }}>
