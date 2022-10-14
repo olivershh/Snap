aabf46319b5f404041718dad041482f12dbb387a
olivershh <oliver.shackleton@gmail.com>
2022-10-14 16:05:31 +0100

    background test

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import { StyleSheet, View } from "react-native";
+import { ImageBackground, StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import CameraView from "../components/CameraView";
 import CameraControls from "../components/CameraControls";
@@ -10,7 +10,10 @@ export default function CameraScreen() {
   const [image, setImage] = useState(null);
 
   return (
-    <SafeAreaView style={{ padding: 15, backgroundColor: "gray", flex: 1 }}>
+    <ImageBackground
+      source={require("../colors.jpg")}
+      style={{ padding: 15, backgroundColor: "gray", flex: 1 }}
+    >
       <View style={{ padding: 15, backgroundColor: "white" }}>
         {!image ? (
           <CameraView cameraRef={cameraRef} />
@@ -20,6 +23,6 @@ export default function CameraScreen() {
       </View>
 
       <CameraControls cameraRef={cameraRef} setImage={setImage} image={image} />
-    </SafeAreaView>
+    </ImageBackground>
   );
 }
