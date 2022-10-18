9e21a61c4f64c5a1fb54bf41d69e049097c169e1
olivershh <oliver.shackleton@gmail.com>
2022-10-18 10:48:24 +0100

    added icons and onfocus to tab bar

--- App.js
+++ App.js
@@ -6,8 +6,8 @@ import Login from "./screens/Login";
 import Profile from "./screens/Profile";
 import AlbumStack from "./stacks/AlbumStack";
 import CameraScreen from "./screens/CameraScreen";
-import SinglePhotoScreen from "./screens/SinglePhotoScreen";
-import GenerateTreeTest from "./screens/GenerateTreeTest";
+import { MaterialIcons, Entypo } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -16,15 +16,34 @@ export default function App() {
   const TabNav = () => {
     return (
       <Tab.Navigator screenOptions={{ headerShown: true }}>
-        <Tab.Screen name="Camera" component={CameraScreen} />
         <Tab.Screen
-          name="AlbumStack"
-          component={AlbumStack}
-          options={{ headerShown: false }}
+          name="Camera"
+          component={CameraScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Entypo name="camera" size={24} color={color} />
+            ),
+          }}
         />
-        <Tab.Screen name="Profile" children={() => <Profile />} />
-        <Tab.Screen name="tree" children={() => <GenerateTreeTest />} />
-        {/* <Tab.Screen name="TempSingle" component={SinglePhotoScreen} /> */}
+        <Tab.Screen
+          name="My Albums"
+          component={AlbumStack}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="photo-album" size={24} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          children={() => <Profile />}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="person" size={24} color={color} />
+            ),
+          }}
+        />
       </Tab.Navigator>
     );
   };
